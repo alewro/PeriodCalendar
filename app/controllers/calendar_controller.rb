bdb7b14c5435185fb4f19dd587a9e8dfbe01344f
Aleksandra <aleksandra@wroblewscy.eu>
2023-03-18 12:25:01 +0100

    Add event icons and split one/more day events.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,7 +2,8 @@ class CalendarController < ApplicationController
 
   def index
     start_date = params.fetch(:start_date, Date.today).to_date
-    @one_days = OneDay.where(start_time: start_date.beginning_of_month..start_date.end_of_month) + MoreDay.where(start_time: start_date.beginning_of_month..start_date.end_of_month)
+    @one_days = OneDay.where(start_time: start_date.beginning_of_month..start_date.end_of_month)
+    @more_days = MoreDay.where(start_time: start_date.beginning_of_month..start_date.end_of_month)
   end
   
 end
